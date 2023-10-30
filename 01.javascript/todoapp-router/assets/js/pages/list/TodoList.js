e9faed7389576906fee71f050ae3c5a2dc1f5acc
zxxng <jeayeong123@naver.com>
2023-10-30 15:57:34 +0900

    Chore: todoapp-router 추가
    Co-authored-by: zxxng <wodud198967@gmail.com>
    Co-authored-by: seonyeongyoon <ssunnyy0204@gmail.com>
    Co-authored-by: wSeungMi <seungmi.dev@gmail.com>
    Co-authored-by: yoonth95 <yoonth0919@gmail.com>

--- 01.javascript/todoapp-router/assets/js/pages/list/TodoList.js
+++ 01.javascript/todoapp-router/assets/js/pages/list/TodoList.js
@@ -1,37 +1,119 @@
 // 할일 목록
 import Header from '../../layout/Header.js';
 import Footer from '../../layout/Footer.js';
-import {linkTo} from '../../Router.js';
+import TodoRegist from '../regist/TodoRegist.js';
+import TodoInfo from '../info/TodoInfo.js';
+import { linkTo } from '../../Router.js';
 
-const TodoList = async function(){
+const TodoList = async function () {
   const page = document.createElement('div');
   page.setAttribute('id', 'page');
 
   const content = document.createElement('div');
   content.setAttribute('id', 'content');
   let response;
-  try{    
-    response = await axios('http://localhost:33088/api/todolist');
+  try {
+    response = await axios('http://localhost:33088/api/todolist?page=1');
 
     const ul = document.createElement('ul');
     ul.setAttribute('class', 'todolist');
-    response.data?.items.forEach(item => {
-      const li = document.createElement('li');
-      const todoInfoLink = document.createElement('a');
-      todoInfoLink.setAttribute('href', `info?_id=${item._id}`);
-      const title = document.createTextNode(item.title);
+    console.log(response);
 
-      todoInfoLink.addEventListener('click', function(event){
-        // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
-        event.preventDefault();
+    // 페이지네이션 추가
+    pagination(response.data?.pagination.totalPages);
+
+    response.data?.items.forEach((item) => {
+      const li = document.createElement('li');
+
+      // 상세조회 버튼
+      const todoInfoLink = document.createElement('a');
+      todoInfoLink.innerText = '상세조회';
+      todoInfoLink.setAttribute('class', 'move_datail');
+      todoInfoLink.setAttribute('href', `info?_id=${item._id}`);
+
+      // 삭제 버튼
+      const todoDelete = document.createElement('button');
+      todoDelete.innerText = '삭제';
+      todoDelete.setAttribute('type', 'button');
+      todoDelete.setAttribute('class', 'move_datail');
+      todoDelete.addEventListener('click', async (e) => {
+        const id = item._id;
+        const li_tag = e.target.parentNode;
+        const ul_tag = li_tag.parentNode;
+
+        if (li_tag) {
+          if (window.confirm('정말 삭제하시겠습니까?')) {
+            ul_tag.removeChild(li_tag);
+
+            // url: /todolist/{_id}
+            // method: DELETE
+            try {
+              const response = await axios.delete(
+                `http://localhost:33088/api/todolist/${id}`,
+              );
+              const data = response.data;
+              console.log(data);
+            } catch (err) {
+              alert('삭제 실패');
+              console.error(err);
+            }
+          }
+        }
+      });
+
+      // 타이틀
+      const titleTag = document.createElement('span');
+      titleTag.innerText = item.title;
+
+      // 완료 체크 박스
+      const completeCheck = document.createElement('input');
+      completeCheck.setAttribute('type', 'checkbox');
+      completeCheck.setAttribute('class', 'check');
+      completeCheck.addEventListener('click', async function (e) {
+        titleTag.style.textDecoration = e.target.checked
+          ? 'line-through'
+          : 'unset';
+
+        // check 후 서버 통신으로 done을 바꾸기
+        // url: /todolist/{_id}
+        // method: DELETE
+        try {
+          const body = {
+            title: item.title,
+            content: item.content,
+            done: e.target.checked,
+          };
+          const response = await axios.patch(
+            `http://localhost:33088/api/todolist/${item._id}`,
+            body,
+          );
+          const data = response.data;
+          console.log(data);
+        } catch (err) {
+          alert('수정 실패');
+          console.error(err);
+        }
+      });
+
+      // 상세조회 버튼 클릭 시 동작
+      todoInfoLink.addEventListener('click', async function (e) {
+        e.preventDefault(); // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
         linkTo(todoInfoLink.getAttribute('href'));
       });
 
-      todoInfoLink.appendChild(title);
+      li.appendChild(completeCheck);
+      li.appendChild(titleTag);
       li.appendChild(todoInfoLink);
+      li.appendChild(todoDelete);
       ul.appendChild(li);
+
+      // 완료 목록 체크
+      if (item.done) {
+        completeCheck.checked = true;
+        titleTag.style.textDecoration = 'line-through';
+      }
     });
-    content.appendChild(ul);    
+    content.appendChild(ul);
 
     const btnRegist = document.createElement('button');
     const btnTitle = document.createTextNode('등록');
@@ -41,16 +123,26 @@ const TodoList = async function(){
     btnRegist.addEventListener('click', () => {
       linkTo('regist');
     });
-
-  }catch(err){
+  } catch (err) {
     const error = document.createTextNode('일시적인 오류 발생');
     content.appendChild(error);
   }
-  
+
   page.appendChild(Header('TODO App 목록 조회'));
   page.appendChild(content);
   page.appendChild(Footer());
   return page;
 };
+
+// pagination 함수
+function pagination(totalPages = 1) {
+  const pageBtns = document.createElement('ul');
+  for (let i = 0; i < totalPages; i++) {
+    const pageBtn = document.createElement('li');
+    const pageNum = document.createTextNode(i + 1);
+    // page;
+    pageBtns.appendChild(pageBtn);
+  }
+}
 
 export default TodoList;
--- 01.javascript/todoapp-router/assets/js/pages/regist/TodoRegist.js
+++ 01.javascript/todoapp-router/assets/js/pages/regist/TodoRegist.js
@@ -1,8 +1,9 @@
 // 할일 등록
 import Header from '../../layout/Header.js';
 import Footer from '../../layout/Footer.js';
+import { linkTo } from '../../Router.js';
 
-const TodoRegist = function(){
+const TodoRegist = function () {
   const page = document.createElement('div');
   page.setAttribute('id', 'page');
 
@@ -10,8 +11,61 @@ const TodoRegist = function(){
   const text = document.createTextNode('등록 화면');
   content.appendChild(text);
 
+  // title
+  const title = document.createElement('div');
+  const titleText = document.createTextNode('제목');
+  const titleInput = document.createElement('input');
+  titleInput.setAttribute('type', 'text');
+  title.appendChild(titleText);
+  title.appendChild(titleInput);
+
+  // content
+  const detail = document.createElement('div');
+  const detailText = document.createTextNode('상세내용');
+  const detailInput = document.createElement('input');
+  detailInput.setAttribute('type', 'text');
+  detail.appendChild(detailText);
+  detail.appendChild(detailInput);
+
+  // button
+  const registBtn = document.createElement('button');
+  const btnText = document.createTextNode('Todo 등록');
+  registBtn.setAttribute('type', 'button');
+  registBtn.appendChild(btnText);
+  registBtn.addEventListener('click', async (e) => {
+    const titleVal = titleInput.value;
+    const detailVal = detailInput.value;
+    console.log(titleVal);
+    console.log(detailVal);
+
+    // 값 체크
+    if (!titleVal || !detailVal) {
+      alert('제목과 상세내용을 모두 입력해주세요!');
+      return;
+    }
+
+    // url: /todolist
+    // method: POST
+    // body: {"title": value, "content": value}
+    try {
+      const body = { title: titleVal, content: detailVal, done: false };
+      const response = await axios.post(
+        'http://localhost:33088/api/todolist',
+        body,
+      );
+      const data = response.data;
+      console.log(data);
+      linkTo('/');
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
   page.appendChild(Header('TODO App 등록'));
   page.appendChild(content);
+  page.appendChild(title);
+  page.appendChild(detail);
+  page.appendChild(registBtn);
   page.appendChild(Footer());
 
   return page;
