602a0662edfda6ec429aa070096a788d6a4f7058
윤태현 <yoonth0919@gmail.com>
2023-10-31 14:10:46 +0900

    Feat: 총 task 수 추가

--- 01.javascript/todoapp-router/assets/js/pages/list/TodoList.js
+++ 01.javascript/todoapp-router/assets/js/pages/list/TodoList.js
@@ -4,17 +4,17 @@ import Footer from '../../layout/Footer.js';
 import Pagination from './Pagination.js';
 import Content from './Content.js';
 import { linkTo } from '../../Router.js';
-import './TodoList.css';
 
 const TodoList = async function() {
   const page = document.createElement('div');
   page.setAttribute('id', 'page');
 
   // header
-  page.appendChild(Header('TODO App 목록 조회'));
+  // page.appendChild(Header('TODO App 목록 조회'));
 
   // 오늘 날짜 조회
   const H2 = document.createElement('h2');
+  H2.setAttribute('class', 'TodoList-H2');
   H2.innerText = getToday();
   page.appendChild(H2);
 
@@ -22,10 +22,18 @@ const TodoList = async function() {
   const pageNum = searchParam('page') || 1;
   const limitNum = searchParam('limit') || 10;
   const newContent = await Content(pageNum, limitNum);
+  const totalPages = newContent.response.data.pagination.totalPages;
+  const totalNum = newContent.response.data.pagination.total;
   page.insertBefore(newContent.content, page.childNodes[2]);  
 
   // 페이지네이션 (totalPage, limitNum, page태그)
-  page.appendChild(Pagination(newContent.response.data.pagination.totalPages || 1, limitNum, page));
+  page.appendChild(Pagination(totalPages || 1, limitNum, page));
+
+  // 총 Task 수
+  const taskNum = document.createElement("p");
+  taskNum.setAttribute('class', 'TodoList-taskNum');
+  taskNum.innerText = `${totalNum} tasks`;
+  page.insertBefore(taskNum, page.childNodes[1]);
 
   // 등록 버튼
   const btnRegist = document.createElement('button');
