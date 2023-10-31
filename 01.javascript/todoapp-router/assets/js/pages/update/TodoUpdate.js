374ab96e6895134c06e8259aa897de91f8bf8180
seonyeongyoon <ssunnyy0204@gmail.com>
2023-10-31 10:39:14 +0900

    Feat:수정하기 페이지 데이터 받아오기

--- 01.javascript/todoapp-router/assets/js/pages/update/TodoUpdate.js
+++ 01.javascript/todoapp-router/assets/js/pages/update/TodoUpdate.js
@@ -3,19 +3,39 @@ import Header from "../../layout/Header.js";
 import Footer from "../../layout/Footer.js";
 import { linkTo } from "../../Router.js";
 
-const TodoUpdate = function () {
-    const page = document.createElement("div");
-    page.setAttribute("id", "page");
+const TodoUpdate = async function () {
+  // const params = new URLSearchParams(location.search);
+  // const _id = params.get("_id");
 
-    const content = document.createElement("div");
-    const text = document.createTextNode("수정하기");
-    content.appendChild(text);
+  const page = document.createElement("div");
+  page.setAttribute("id", "page");
 
-    page.appendChild(Header("TODO App 수정하기"));
-    page.appendChild(content);
-    page.appendChild(Footer());
+  const content = document.createElement("div");
+  const text = document.createTextNode("수정하기");
+  content.appendChild(text);
 
-    return page;
+  let response;
+
+  //초기 content
+  const _id = searchParam("_id");
+  console.log(_id);
+
+  //쿼리스트링 값 가져오기
+  function searchParam(key) {
+    return new URLSearchParams(location.search).get(key);
+  }
+
+  try {
+    response = await axios(`http://localhost:33088/api/todolist/${_id}`);
+    const data = response.data.item;
+    console.log(data);
+  } catch (error) {}
+
+  page.appendChild(Header("TODO App 수정하기"));
+  page.appendChild(content);
+  page.appendChild(Footer());
+
+  return page;
 };
 
 export default TodoUpdate;
