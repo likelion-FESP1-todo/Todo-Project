043d5588d9bf48c177dd7ed355e25a6b08dd557d
zxxng <jeayeong123@naver.com>
2023-11-10 13:27:59 +0900

    Style(React): index.css 적용

--- 03.react/todoapp/src/Pages/TodoList/TodoList.tsx
+++ 03.react/todoapp/src/Pages/TodoList/TodoList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from '../Layout/Footer';
-import Header from '../Layout/Header';
+import Footer from '../../Layout/Footer';
+import Header from '../../Layout/Header';
 
 const TodoList = function () {
   return (
-    <>
+    <div id="page">
       <Header title="할일 목록 페이지" />
       <Footer />
-    </>
+    </div>
   );
 };
 
--- 03.react/todoapp/src/Pages/TodoRegist/TodoRegist.tsx
+++ 03.react/todoapp/src/Pages/TodoRegist/TodoRegist.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from '../Layout/Footer';
-import Header from '../Layout/Header';
+import Footer from '../../Layout/Footer';
+import Header from '../../Layout/Header';
 
 const TodoRegist = function () {
   return (
-    <>
+    <div id="page">
       <Header title="할일 등록 페이지" />
       <Footer />
-    </>
+    </div>
   );
 };
 
