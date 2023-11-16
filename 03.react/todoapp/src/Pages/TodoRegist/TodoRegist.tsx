97bb9ec5e7644590bbc8bc34bf0bf3e3ce33547c
zxxng <jeayeong123@naver.com>
2023-11-16 14:49:09 +0900

    Feat(React): 등록페이지 추가

--- 03.react/todoapp/src/Pages/TodoRegist/TodoRegist.tsx
+++ 03.react/todoapp/src/Pages/TodoRegist/TodoRegist.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../../Layout/Footer';
 import Header from '../../Layout/Header';
+import Button from 'Components/Utils/Button';
+import { useBackEvent } from 'Components/Utils/utilsFunction';
+import styles from '../TodoRegistAndUpdate.module.css';
 
 const TodoRegist = function () {
+  const backEvent = useBackEvent();
   return (
     <div id="page">
+      <Header
+        title={'할 일 등록'}
+        className={styles['Todo-header']}
+      >
+        <Button
+          svg={'backButton'}
+          alt={'뒤로가기'}
+          btnEvent={backEvent}
+        />
+      </Header>
       <Footer />
     </div>
   );
