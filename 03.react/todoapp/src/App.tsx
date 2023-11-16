b44e43ff8f52fc0a8dca2772f1c53edac841a3e9
zxxng <jeayeong123@naver.com>
2023-11-16 10:38:35 +0900

    fix: App router 상세조회 페이지 링크 수정

--- 03.react/todoapp/src/App.tsx
+++ 03.react/todoapp/src/App.tsx
@@ -9,10 +9,22 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<TodoList />} />
-        <Route path='/list' element={<TodoInfo />} />
-        <Route path='/regist' element={<TodoRegist />} />
-        <Route path='/update' element={<TodoUpdate />} />
+        <Route
+          path="/"
+          element={<TodoList />}
+        />
+        <Route
+          path="/info"
+          element={<TodoInfo />}
+        />
+        <Route
+          path="/regist"
+          element={<TodoRegist />}
+        />
+        <Route
+          path="/update"
+          element={<TodoUpdate />}
+        />
       </Routes>
     </Router>
   );
